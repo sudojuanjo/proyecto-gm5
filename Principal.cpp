e1ffcd3b1f673950e79033d5e26dd4cc17ff9cd5
AngelSM11 <71821459+AngelSM11@users.noreply.github.com>
2020-12-20 13:32:01 +0100

    Update Principal.cpp

--- Principal.cpp
+++ Principal.cpp
@@ -8,6 +8,41 @@ cout<<"Su rol en la empresa es:"<<endl<<"1-Administrativo"<<endl<<"2-Monitor"<<e
 cin>>opcion;
 switch(opcion) {
 case 1: 
+    switch(opc_adm) {
+        case 1: 
+
+
+
+        ;break;
+
+        case 2:
+
+
+        ;break;
+
+        case 3: 
+
+
+
+        ;break;
+
+        case 4:
+
+
+        ;break;
+
+        case 5: 
+
+
+
+        ;break;
+
+        case 6:
+
+
+        ;break;
+
+
 
 
 ;break;
