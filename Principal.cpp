77127b18f5ac8bc9624abe1f3051b8bd54a3e2a0
i82rusam <71816043+i82rusam@users.noreply.github.com>
2020-12-20 16:20:35 +0100

    Update Principal.cpp
    Arreglo de errores y principio de caso 2 (monitor)

--- Principal.cpp
+++ Principal.cpp
@@ -1,82 +1,125 @@
 #include <stdio.h>
 #include <iostream>
+#include "ClaseAdministrativo.h"
+#include "ClaseCliente.h"
+#include "ClaseMonitor.h"
+#include "ClaseParque.h"
+#include "ClaseRuta.h"
+#include "ClaseSendero.h"
 using namespace std;
 
-int main() {
-int opcion;
-int opc_adm;
-cout<<"Su rol en la empresa es:"<<endl<<"1-Administrativo"<<endl<<"2-Monitor"<<endl;
-cin>>opcion;
-switch(opcion) {
-case 1:
-    while(opc_adm<7){
-        cout<<"Eliga la funcion a realizar:"<<endl<<
-        "1-Consultar disponibilidad de ruta"<<endl<<
-        "2-Crear calendario"<<endl<<
-        "3-Consultar calendario"<<endl<<
-        "4-Guardar cliente"<<endl<<
-        "5-Visualizar grupo de ruta"<<endl<<
-        "6-Acceder a la lista de monitores"<<endl<<
-        "7-Salir del programa"<<endl;
-        cin>>opc_adm;
-    switch(opc_adm) {
-        case 1: 
-        //utilizamos la funcion consultar con el fichero de disponibilidad
-        string disponibilidad = consultar(string disponibilidad.txt);
-        cout<< "la disponibilidad de la ruta es:"<< disponibilidad << endl;
+int main() 
+{
+    int opcion;
+    int opc_adm=0, int opc_mon=0;
+    
+    cout<<"Su rol en la empresa es:"<<endl<<"1-Administrativo"<<endl<<"2-Monitor"<<endl;
+    cin>>opcion;
+    
+    switch(opcion)
+    {
+        case 1:
+            while(opc_adm<7)
+            {
+                cout<<"Eliga la funcion a realizar:"<<endl<<
+                "1-Consultar disponibilidad de ruta"<<endl<<
+                "2-Crear calendario"<<endl<<
+                "3-Consultar calendario"<<endl<<
+                "4-Guardar cliente"<<endl<<
+                "5-Visualizar grupo de ruta"<<endl<<
+                "6-Acceder a la lista de monitores"<<endl<<
+                "7-Salir del programa"<<endl;
+                cin>>opc_adm;
+                
+                switch(opc_adm) 
+                {
+                    case 1: 
+                        //utilizamos la funcion consultar con el fichero de disponibilidad
+                        string disponibilidad = consultar(string disponibilidad.txt);
+                        cout<< "la disponibilidad de la ruta es:"<< disponibilidad << endl;
+                    ;break;
+
+                    case 2:
+                        bool i=crear_cal();
+                        if(i==-1)
+                        {
+                            cout<<"el calendario no se ha podido crear"<<endl;
+                        }
+                        else
+                        {
+                            cout<<"el calendario se ha creado satisfactoriamente"<<endl;
+                        }
+                    ;break;
+
+                    case 3: 
+                        bool j=consultar_cal();
+                        if(j==-1)
+                        {
+                            cout<<"el calendario no se ha podido abrir"<<endl;
+                        }
+                    ;break;
+
+                    case 4:
+                        introduccir(clientes.txt);
+                        cout<<"Cliente guardado con exito"<<endl;
+                    ;break;
+
+                    case 5: 
+                        string grup=consultar(grupo.txt);
+                        if(grup=="-1")
+                        {
+                            cout<<"ERROR"<<endl;
+                        }
+                        else
+                        {
+                            cout<<grup<<endl;
+                        }
+                    ;break;
+
+                    case 6:
+                        string moni=consultar(monitores.txt);
+                        if(moni=="-1")
+                        {
+                            cout<<"ERROR"<<endl;
+                        }
+                        else
+                        {
+                            cout<<moni<<endl;
+                        }
+                    ;break;
+
+                    case 7:
+                        return 0;
+                    ;break
+                }
+            }
         ;break;
 
         case 2:
-        bool i=crear_cal();
-        if(i==-1){
-            cout<<"el calendario no se ha podido crear"<<endl;
-        }
-        else{
-            cout<<"el calendario se ha creado satisfactoriamente"<<endl;
-        }
-        ;break;
+            while(opc_mon<4)
+            {
+                cout<<"Eliga la funcion a realizar:"<<endl<<
+                "1-Consultar calentario de rutas"<<endl<<
+                "2-Introducir informacion sobre la ruta"<<endl<<
+                "3-Registrar horas trabajadas"<<endl<<
+                "4-Salir del programa"<<endl;
+                cin>>opc_mon;
 
-        case 3: 
-        bool j=consultar_cal();
-        if(j==-1){
-            cout<<"el calendario no se ha podido abrir"<<endl;
-        }
-        ;break;
-
-        case 4:
-            introduccir(clientes.txt);
-            cout<<"Cliente guardado con exito"<<endl;
-
-        ;break;
-
-        case 5: 
-            string grup=consultar(grupo.txt);
-            if(grup=="-1"){cout<<"ERROR"<<endl;}
-            else{cout<<grup<<endl;}
+                switch(opc_mon) 
+                {
+                    case 1:
+                        bool k=consultar_cal();
+                        if(k==-1)
+                        {
+                            cout<<"el calendario no se ha podido abrir"<<endl;
+                        }
+                    ;break;
+                }
+            }
 
         ;break;
 
-        case 6:
-            string moni=consultar(monitores.txt);
-            if(moni=="-1"){cout<<"ERROR"<<endl;}
-            else{cout<<moni<<endl;}
-
-        ;break;
-
-        case 7:
-            return 0;
-        ;break
+        default: cout<<"ERROR";
+        break;
     }
-
-
-
-;break;
-
-case 2:
-
-
-;break;
-
-default: cout<<"ERROR";
-break;
-}}
+}
