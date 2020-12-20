1e60058e0bd4483b624369429d3d0ced806d6280
sudojuanjo <71605321+sudojuanjo@users.noreply.github.com>
2020-12-20 12:34:09 +0100

    Create Calendario.cpp
    He creado dos funciones, una crea el calendario añadiendo una linea con cada evento ,especificando el día y una descripción, y la otra muestra por pantalla todas las líneas del documento "calendario.txt" para visualizar todos los eventos.

---
+++ Calendario.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include "ClaseParque.h"
+#include "ClaseRuta.h"
+#include "ClaseSendero.h"
+#include "ClaseAdministrativo.h"
+#include "ClaseMonitor.h"
+#include "ClaseCliente.h"
+using namespace std;
+
+void crear_cal() {
+    ofstream file;
+    file.open(calendario.txt);
+    if(file.is_open()==false) {
+        cout<<"El fichero no se pudo abrir"<<endl;
+        
+    }
+    string evento;
+    cout<<"Introduzca un dia y sus eventos"<<endl;
+    cin<<evento;
+    file << evento;
+}
+
+string consultar_cal() {
+    ifstream file;
+    file.open(calendario.txt);
+    if(file.is_open()==false) {
+        cout<<"El fichero no se pudo abrir"<<endl;
+        return -1;
+    }
+    string calendario;
+    while (file.eof()==false) {
+        file >> calendario;
+        cout << calendario << endl;
+    }
+    return calendario;
+}
