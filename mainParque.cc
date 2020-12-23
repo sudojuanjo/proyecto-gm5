c4ee60fb9c9587b21f60d888fc83405f975bac02
i82rusam <71816043+i82rusam@users.noreply.github.com>
2020-12-23 12:06:10 +0100

    prueba clase parque
    pruebas con main de la clase parque

---
+++ mainParque.cc
@@ -0,0 +1,68 @@
+#include "ClaseParque.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    Parque p;
+    
+	string nombre;
+    string municipios;
+    string direccion;
+    string localidad;
+    float superficie;
+    int telefono;
+    string horario;
+    string men_prem;
+    string fech_natural;
+
+    cout<<"Introduzca nombre"<<endl;
+    cin>>nombre;
+    cout<<"Introduzca municipios"<<endl;
+    cin>>municipios;
+    cout<<"Introduzca direccion"<<endl;
+    cin>>direccion;
+	cout<<"Introduzca localidad"<<endl;
+    cin>>localidad;
+	cout<<"Introduzca superficie"<<endl;
+    cin>>superficie;
+	cout<<"Introduzca telefono"<<endl;
+    cin>>telefono;
+    cout<<"Introduzca horario"<<endl;
+    cin>>horario;
+	cout<<"Introduzca men_prem"<<endl;
+    cin>>men_prem;
+    cout<<"Introduzca fech_natural"<<endl;
+    cin>>fech_natural;
+
+    p.setNombre(nombre);
+    p.setMunicipios(municipios);
+    p.setDireccion(direccion);
+    p.setLocalidad(localidad);
+    p.setSuperficie(superficie);
+    p.setTelefono(telefono);
+    p.setHorario(horario);
+    p.setMen_prem(men_prem);
+    p.setFech_natural(fech_natural);
+
+    ofstream salida("Parques.txt");
+
+	if(!salida)
+	{
+		cout << "Error al abrir el fichero." << endl;
+		return 0;
+	}
+
+	salida << p.getNombre() << "," << p.getMunicipios() << "," << p.getDireccion() << "," << p.getLocalidad()<< ","<< p.getSuperficie()<<","<<p.getTelefono()<<","<<p.getHorario()<<","<<p.getMen_prem()<<","<<p.getFech_natural()<<endl;
+
+	salida.close();
+
+	p.introducirInfo("Parques.txt");
+	p.guardar("Parques.txt");
+
+
+	p.consultar("Parques.txt");
+
+
+	return 0;
+}
